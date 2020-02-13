050a6b5bcf9b6d7a5c5a7877806261b733e19687
Martin <mail2maras@gmail.com>
2020-02-13 17:37:46 +0100

    changes

--- Assets/Scripts/Authoring/GameData/GameDataSO.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObjects/GameSettingsSO", order = 1)]
-public class GameDataSO : ScriptableObject
-{
-    public GameData GameData;
-}
