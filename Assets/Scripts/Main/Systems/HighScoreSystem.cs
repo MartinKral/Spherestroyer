5cda56985404e865e8ae5f756474da2d54a320eb
Martin <mail2maras@gmail.com>
2021-02-26 14:35:06 +0100

    Implemented GameSaveSystem

--- Assets/Scripts/Main/Systems/HighScoreSystem.cs
+++ Assets/Scripts/Main/Systems/HighScoreSystem.cs
@@ -1,49 +1,50 @@
-﻿using Unity.Collections;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Jobs;
 
+[UpdateAfter(typeof(GameSaverSystem))]
 [AlwaysSynchronizeSystem]
 public class HighScoreSystem : SystemBase
 {
     public int CurrentHighscore => savedHighscore;
-    private readonly SavedInt savedHighscore = new SavedInt("spherestroyer-leaderboard");
+
+    private readonly string highscoreSaveKey = "highscore";
+    private int savedHighscore = 0;
+    private GameSaverSystem gameSaverSystem;
+
+    private bool hasReadScore = false;
 
     protected override void OnCreate()
     {
         RequireSingletonForUpdate<GameData>();
-    }
-
-    protected override void OnStartRunning()
-    {
-        if (savedHighscore == 0)
-        {
-            // EntityManager.AddComponent(highScoreUi, typeof(Disabled));
-            Logger.Log("Disabling highscore");
-        }
+        gameSaverSystem = World.GetExistingSystem<GameSaverSystem>();
     }
 
     protected override void OnUpdate()
     {
+        if (gameSaverSystem.IsReady && !hasReadScore)
+        {
+            hasReadScore = true;
+            gameSaverSystem.Read(highscoreSaveKey, ref savedHighscore);
+        }
+
         var gameData = GetSingleton<GameData>();
 
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-
         Entities
-            .WithoutBurst()
             .WithAll<UpdateHighscoreTag>()
             .ForEach((Entity entity) =>
             {
                 if (savedHighscore < gameData.score)
                 {
-                    ecb.AddComponent(ecb.CreateEntity(), new SoundRequest { Value = SoundType.Highscore });
-                    savedHighscore.Value = gameData.score;
+                    EntityManager.AddComponentData(EntityManager.CreateEntity(), new SoundRequest { Value = SoundType.Highscore });
+                    savedHighscore = gameData.score;
+
+                    gameSaverSystem.Write(highscoreSaveKey, savedHighscore);
+                    gameSaverSystem.Save();
                 }
 
                 Y8.Api.SaveHighscore("Leaderboard", gameData.score);
 
-                ecb.DestroyEntity(entity);
-            }).Run();
-        ecb.Playback(EntityManager);
-        ecb.Dispose();
+                EntityManager.DestroyEntity(entity);
+            }).WithStructuralChanges().Run();
     }
 }
---
+++ Assets/Scripts/Main/Utils/GameSaver/GameSaverSystem.cs
@@ -0,0 +1,125 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+#if UNITY_DOTSRUNTIME
+using Unity.Tiny.GameSave;
+
+[AlwaysUpdateSystem]
+[UpdateAfter(typeof(GameSaveSystem))]
+#endif
+
+public class GameSaverSystem : SystemBase
+{
+    public bool IsReady { get; private set; } = false;
+
+#if UNITY_DOTSRUNTIME
+    private readonly string mainSaveFileName = "main-save-file";
+    private GameSaveSystem gameSaveSystem;
+
+    protected override void OnCreate()
+    {
+        gameSaveSystem = World.GetExistingSystem<GameSaveSystem>();
+    }
+
+    protected override void OnStartRunning()
+    {
+        EntityManager.AddComponentData(
+            EntityManager.CreateEntity(),
+            new GameSaveReadFromPersistentStorageRequest(mainSaveFileName, 0));
+    }
+#else
+    private GameSaverSystemMock gameSaveSystem;
+
+    protected override void OnCreate()
+    {
+        gameSaveSystem = World.GetExistingSystem<GameSaverSystemMock>();
+        IsReady = true;
+    }
+
+#endif
+
+    protected override void OnUpdate()
+    {
+        if (IsReady) return;
+
+#if UNITY_DOTSRUNTIME
+        Entities.ForEach((Entity e, ref GameSaveReadFromPersistentStorageRequest readRequest) => {
+            if ((GameSaverResult)(int)readRequest.result == GameSaverResult.Success)
+            {
+                IsReady = true;
+            }
+        }).WithStructuralChanges().Run();
+#endif
+    }
+
+    public GameSaverResult Read<T>(string key, ref T value) where T : unmanaged
+    {
+        return (GameSaverResult)(int)gameSaveSystem.Read(key, ref value);
+    }
+
+    public GameSaverResult Read<T>(string key, out T value, in T defaultValue) where T : unmanaged
+    {
+        return (GameSaverResult)(int)gameSaveSystem.Read(key, out value, defaultValue);
+    }
+
+    public GameSaverResult Write<T>(string key, T value) where T : unmanaged
+    {
+        return (GameSaverResult)(int)gameSaveSystem.Write(key, value);
+    }
+
+    public void Save()
+    {
+#if UNITY_DOTSRUNTIME
+        EntityManager.AddComponentData(
+            EntityManager.CreateEntity(),
+            new GameSaveWriteToPersistentStorageRequest(mainSaveFileName, 0));
+#endif
+    }
+}
+
+public class GameSaverSystemMock : SystemBase
+{
+    protected override void OnUpdate()
+    {
+    }
+
+    public GameSaverResult Read<T>(FixedString64 key, ref T value) where T : unmanaged
+    {
+        Logger.Log($"[FAKE] Tried to read {key}");
+        return GameSaverResult.Success;
+    }
+
+    public GameSaverResult Read<T>(FixedString64 key, out T value, in T defaultValue) where T : unmanaged
+    {
+        Logger.Log($"[FAKE] Tried to read {key}");
+        value = defaultValue;
+        return GameSaverResult.Success;
+    }
+
+    public GameSaverResult Write<T>(FixedString64 key, T value) where T : unmanaged
+    {
+        Logger.Log($"[FAKE] Tried to write {key}");
+        return GameSaverResult.Success;
+    }
+}
+
+public class MockGameSaveReadFromPersistentStorageRequest
+{
+    public GameSaverResult result;
+}
+
+public enum GameSaverResult
+{
+    NotStarted,
+    ReadInProgress,
+    WriteInProgress,
+    Success,
+    ErrorNotFound,
+    ErrorType,
+    ErrorDataSize,
+    ErrorReadFailed,
+    ErrorMultipleRequestsToSameFile,
+    ErrorWriteFailed
+}
